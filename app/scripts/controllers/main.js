b12695bfdb6c6574b449b4a469920ffdf6ae5c30
Greg Dziemidowicz <greg@cogent.co>
2014-08-24 20:12:25 +1000

    skils by type

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,20 @@
 angular.module('SkillsRadarApp')
   .controller('MainCtrl', function ($scope) {
 
+    $scope.types = ["Backend", "Frontend"]
+
+    $scope.skills = [
+        {
+            "name": "Rails",
+            "type": "Backend"
+        },
+        {
+            "name": "Angular",
+            "type": "Frontend"
+        }
+
+    ]
+
     $scope.item_id = "";
 
     $scope.change = function() {
