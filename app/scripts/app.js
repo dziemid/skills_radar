70ca4ae0af836ed21eb47b68f8e10f213601b01f
Greg Dziemidowicz <greg@cogent.co>
2014-08-24 19:59:46 +1000

    empty project ready to go

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular
+  .module('SkillsRadarApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('SkillsRadarApp')
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.item_id = "";
+
+    $scope.change = function() {
+
+    };
+
+  });
